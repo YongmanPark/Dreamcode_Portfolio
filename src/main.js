60920ad40dd47d4832de0f45ba9fcc62acd161ab
YongmanPark <yongman514@gmail.com>
2023-07-04 12:55:44 +0900

    JS HOME

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-console.log("headeerHeight:" + headerHeight);
 document.addEventListener('scroll', () => {
-    console.log(window.scrollY);
     if(window.scrollY > headerHeight){
         header.classList.add("header--dark");
     } else {
         header.classList.remove("header--dark");
     }
+})
+
+// When home section is changed by scroll then change opacity
+const home = document.querySelector(".home_container");
+const homeHeight = home.offsetHeight;
+document.addEventListener("scroll", ()=>{
+    console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 })
